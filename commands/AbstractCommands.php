94a5f45c94c9c33a71ad7a742a04feeff19afafa
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-05-16 15:06:02 +0200

    add method in abstract command class

--- commands/AbstractCommands.php
+++ commands/AbstractCommands.php
@@ -7,4 +7,8 @@ abstract class AbstractCommand {
 
   abstract public function execute(array $option = []);
 
+  protected function delete_same_category_options(array $options, array $same_category_options) {
+    return array_diff($options, $same_category_options);
+  }
+
 }
